40e5fa1654e8034dd3e3cbf6fafb3d096538247f
Lcharvol <charvolin0@gmail.com>
2018-10-16 20:06:21 +0200

    Enhance autoComplete

--- src/client/shortcuts/autoComplete.js
+++ src/client/shortcuts/autoComplete.js
@@ -7,21 +7,30 @@ import {
 } from 'ramda';
 import { supportedCommands } from '../constants/commands';
 
-const autoCompleteArgs = (words, value, existingArgs) => {
-    return `${words[0]} file_name`;
+const autoCompleteArgs = (words, existingArgs) => {
+    let newValue = "";
+
+    map(arg => {
+        let { value } = arg
+        var re = new RegExp(words[1],"g");
+
+        if(length(match(re, value)) > 0 && ((length(value) < length(newValue)) || length(newValue) === 0))
+            newValue = value;
+    },existingArgs);
+    return `${words[0]} ${newValue}`;
 }
 
 const autoCompleteCommand = value => {
-    let newValue = "";
+    let newValue = value;
 
     map(command => {
         const { name } = command;
         var re = new RegExp(value,"g");
 
-        if(length(match(re, name)) > 0 && ((length(name) < length(newValue)) || length(newValue) === 0))
+        if(length(match(re, name)) > 0 && ((length(name) < length(newValue)) || value === newValue))
             newValue = name;
     },supportedCommands);
-    return newValue;
+    return `${newValue} `;
 };
 
 const autoComplete = (value, existingArgs) => {
@@ -29,7 +38,7 @@ const autoComplete = (value, existingArgs) => {
     if(length(words) === 1)
         return autoCompleteCommand(words[0]);
     else if(length(words) === 2)
-        return autoCompleteArgs(words, value, existingArgs);
+        return autoCompleteArgs(words, existingArgs);
 }
 
 export default autoComplete;
